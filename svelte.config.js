784579d16b4e79f2881f73cbd740806ce28c31ed
Juan Villacorta <juanvillacortac@gmail.com>
2022-02-02 14:44:24 -0400

    Target removed

--- svelte.config.js
+++ svelte.config.js
@@ -32,7 +32,6 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#main',
 		methodOverride: {
 			parameter: '_method',
 			allowed: ['PUT', 'PATCH', 'DELETE']
